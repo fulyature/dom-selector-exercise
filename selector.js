6e8bb94569c0f0d8018cd622bbbfb86540e93cff
fulya_ture <fulyature@gmail.com>
2023-10-04 21:37:05 +0300

    değer atama

---
+++ selector.js
@@ -0,0 +1,9 @@
+const addButon = document.getElementById("btn");
+console.log(addButon);
+addButon.style.backgroundColor = "#dedede";
+addButon.style.border = "none";
+addButon.style.borderRadius = "5px";
+addButon.style.padding = "8px";
+
+//*değer atama
+addButon.value = "SAVE";
